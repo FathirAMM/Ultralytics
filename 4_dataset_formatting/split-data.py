c3823df7a56d138686937e6ecf6f89fe4a68ec3a
FathirAMM <fathir.majeed@gmail.com>
2025-06-29 13:54:39 +0530

    data set formatting

---
+++ 4_dataset_formatting/split-data.py
@@ -0,0 +1,18 @@
+from zipfile import ZipFile
+import os
+import splitfolders
+
+def unzip_file(zip_path, extract_to):
+    """Extracts a ZIP file."""
+    with ZipFile(zip_path,
+                 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+
+# unzip_file("export.zip",
+#            "export")
+
+# split the dataset into training, validation, and test sets
+# splitfolders.ratio("export",
+#                    "dataset",
+#                    (0.7, 0.15, 0.15))
+
